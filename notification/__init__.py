141d7967c6b2e99d2f912fcbf9c4dc160e4080e6
TuanTL1 <TuanTL1@fpt.com>
2023-10-21 10:00:43 +0700

    update1

--- notification/__init__.py
+++ notification/__init__.py
@@ -7,7 +7,6 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 def main(msg: func.ServiceBusMessage):
-
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
     logging.info('test3 %s',notification_id)
