c6b694ec2438c904bf7df202c8bf39502d76392c
TuanTL1 <TuanTL1@fpt.com>
2023-10-21 09:32:28 +0700

    update

--- notification/__init__.py
+++ notification/__init__.py
@@ -10,22 +10,21 @@ def main(msg: func.ServiceBusMessage):
 
     notification_id = int(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
-
     conn = psycopg2.connect(dbname="techconfdb", user="longtuan03@postgresql-db-sv-tlt-proj3",password="Dragon03", host="postgresql-db-sv-tlt-proj3.postgres.database.azure.com")
-    cursor = connection.cursor()
+    cursor = conn.cursor()
     try:
         notification_query = cursor.execute("SELECT message, subject FROM notification WHERE id = {};".format(notification_id))
-
-        # cursor.execute("SELECT first_name, last_name, email FROM attendee;")
-        # attendees = cursor.fetchall()
-
-        # notification_completed_date = datetime.utcnow()
-
-        # notification_status = 'Notified {} attendees'.format(len(attendees))
-        
-        # update_query = cursor.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notification_status, notification_completed_date, notification_id))        
-
-        # conn.commit()
+        logging.info('test1 %s',notification_query)
+        cursor.execute("SELECT first_name, last_name, email FROM attendee;")
+        attendees = cursor.fetchall()
+        logging.info('test2 %s',attendees)
+        notification_completed_date = datetime.utcnow()
+        logging.info('test4 %s',notification_completed_date)
+        notification_status = 'Notified {} attendees'.format(len(attendees))
+        logging.info('test5 %s',notification_status)
+        update_query = cursor.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(notification_status, notification_completed_date, notification_id))        
+        logging.info('test6 %s',update_query)
+        conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
     finally:
